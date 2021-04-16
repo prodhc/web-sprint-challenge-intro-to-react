3c09c3d6ca7237e0025c3ecbebf60f7f6999c39d
prodhc <zionywood@gmail.com>
2021-04-16 12:00:38 -0700

    updated src and comp

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
+import CharactersCard from './components/CharactersCard';
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+// This is the main page to display the web application
+function App() {
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+	// this will store and set the main data, Star Wars characters retrieved from the axios request
+	const [swCharacters, setSWCharacters] = useState([]); //
+	// Fetch characters from the API in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any. hi.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+	//this will pull the star wars data and save it to the swCharacters state
+	useEffect(() => {
+		axios
+			.get('https://swapi.dev/api/people')
+			.then(res => {
+				setSWCharacters(res.data);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}, []);
+
+
+	return (
+		<div className="App">
+			<Card>
+				<CharactersCard swCharacters={swCharacters} />
+			</Card>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,16 @@
 // Write your Character component here
+import React from 'react';
+
+export default function Character(props) {
+	const { people } = props;
+	// console.log('A');
+	if (!people) return <h2>Loading.....</h2>;
+	// console.log(people);
+	// console.log(props);
+
+	return (
+		<>
+			<h3>Name: {people[0]['name']}</h3>
+		</>
+	);
+}
