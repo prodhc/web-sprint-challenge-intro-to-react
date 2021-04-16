093f5377cc422316e65ac2df7521092ffde1eade
prodhc <zionywood@gmail.com>
2021-04-16 12:11:23 -0700

    updated/finished

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,16 +1,25 @@
-// Write your Character component here
 import React from 'react';
+import styled from 'styled-components';
 
 export default function Character(props) {
-	const { people } = props;
-	// console.log('A');
-	if (!people) return <h2>Loading.....</h2>;
-	// console.log(people);
-	// console.log(props);
+	const { swCharacter } = props;
+
+	// styling for the info in each list item card
+	const Individual = styled.div`
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: 0 5%;
+		text-shadow: 5px 5px 10px #5c5c5c;
+		font-size: 1.5rem;
+	`;
 
 	return (
 		<>
-			<h3>Name: {people[0]['name']}</h3>
+			<Individual>
+				<h3>{swCharacter.name}</h3>
+				<h3>{swCharacter.birth_year}</h3>
+			</Individual>
 		</>
 	);
 }
--- src/components/CharactersCard.js
+++ src/components/CharactersCard.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import Character from './Character';
+import styled from 'styled-components';
 
 export default function CharactersCard(props) {
-	const { people } = props;
+	const { swCharacters } = props;
 
-	if (!people) return <h2>Loading.....</h2>;
+	// safety measure, so that nothing trys to render before the request is finished
+	if (!swCharacters) return <h2>Loading.....</h2>;
+
+	// styling for the list item cards
+	const ListItem = styled.div`
+		background-color: rgba(255, 255, 255, 0.3);
+		border-radius: 20px;
+		margin: 1rem auto;
+		width: 80%;
+		color: white;
+		box-shadow: 5px 5px 10px #5c5c5c;
+	`;
 
 	return (
 		<>
-			<h1 className="Header">Characters</h1>
-			{people.map((person, index) => {
-				return <Character key={index} person={person} />;
+			<h1 className="Header">Star Wars Characters</h1>
+			{swCharacters.map((swCharacter, index) => {
+				return (
+					<ListItem>
+						<Character key={index} swCharacter={swCharacter} />
+					</ListItem>
+				);
 			})}
 		</>
 	);
